f8c33701ca2eeb4810e6968b4414611c7fc8a3c5
ARahimiQuant <a.rahimi.aut@gmail.com>
2023-09-15 23:48:24 +0330

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyfedwatch',
+    version='1.0.0',
+    description='Python Implementation of the CME FedWatch Tool for Estimating Probabilities of Federal Funds Rate Changes at Upcoming FOMC Meetings.',
+    author='ALI RAHIMI',
+    author_email='a.rahimi.aut@gmail.com',
+    url='https://github.com/ARahimiQuant/pyfedwatch',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=2.1.0',
+        'numpy>=1.25.2',
+        'python-dateutil>=2.8.2',
+        'pytz>=2023.3.post1',
+        'tzdata>=2023.3',
+    ],
+    license='Apache-2.0',
+)
