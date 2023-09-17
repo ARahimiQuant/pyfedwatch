d78df20e4a1507445ed47f9e494174edafa87ffa
ARahimiQuant <a.rahimi.aut@gmail.com>
2023-09-17 10:53:40 +0330

    uodate setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pyfedwatch',
-    version='1.0.0',
+    version='1.0.1',
     description='Python Implementation of the CME FedWatch Tool for Estimating Probabilities of Federal Funds Rate Changes at Upcoming FOMC Meetings.',
     author='ALI RAHIMI',
     author_email='a.rahimi.aut@gmail.com',
